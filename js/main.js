7dfb80a7449f1feb666e7b8db411c5be460f85e9
mjid1989 <44952338+mjid1989@users.noreply.github.com>
2019-01-27 03:26:39 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 var GitHub = require('./github');
 var config = {
    username: 'naitlyassemjid@gmail.com',
-   password: 'Mjid79668011', // Either your password or an authentication token if two-factor authentication is enabled
+   password: '', // Either your password or an authentication token if two-factor authentication is enabled
    auth: 'basic',
    repository: 'test2',
    branchName: 'test2'
